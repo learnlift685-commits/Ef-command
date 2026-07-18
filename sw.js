8e9e7a7c0cea914a8f983ccde533891cc51588c9
learnlift685-commits <learnlift685@gmail.com>
2026-07-18 21:56:26 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,5 @@
-const CACHE="ef-command-ultimate-v3";
+const CACHE="ef-command-ultimate-v4";
 const CORE=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png","./icon-512-maskable.png","./apple-touch-icon.png"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)));self.skipWaiting()});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
-self.addEventListener("fetch",e=>{
-  const url=new URL(e.request.url);
-  if(url.hostname.endsWith("wikipedia.org")||url.hostname.endsWith("wikibooks.org")){
-    e.respondWith(fetch(e.request).catch(()=>new Response(JSON.stringify({query:{pages:{}}}),{headers:{"Content-Type":"application/json"}})));return;
-  }
-  e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res}).catch(()=>caches.match("./index.html"))))
-});
+self.addEventListener("fetch",e=>{const u=new URL(e.request.url);if(u.hostname.endsWith("wikipedia.org")||u.hostname.endsWith("wikibooks.org")){e.respondWith(fetch(e.request).catch(()=>new Response(JSON.stringify({query:{pages:{}}}),{headers:{"Content-Type":"application/json"}})));return}e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(cache=>cache.put(e.request,copy));return r}).catch(()=>caches.match("./index.html"))))});
